e5c874832e250c290844e361ce6c0431e6637af7
arecastudio <ondiisrail@gmail.com>
2017-08-06 21:00:48 +0900

    project baru

--- app/src/main/java/io/github/arecastudio/jayapurafjb/Main.java
+++ app/src/main/java/io/github/arecastudio/jayapurafjb/Main.java
@@ -3,9 +3,6 @@ package io.github.arecastudio.jayapurafjb;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,8 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import io.github.arecastudio.jayapurafjb.fragements.Kategori;
 
 public class Main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -88,11 +83,7 @@ public class Main extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-            Fragment fragment = new Kategori();
-            FragmentManager fm = getSupportFragmentManager();
-            FragmentTransaction transaction = fm.beginTransaction();
-            transaction.replace(R.id.contentFragment, fragment); //Container -> R.id.contentFragment
-            transaction.commit();
+
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
